b68ec371d83fc5076fc9e2af4d26c6b73878b833
Kuan <kuan@hey.com>
2021-04-25 22:50:21 +0800

    greet birthday

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,35 @@
 <script>
-  import { onMount } from 'svelte'
-  export let name
-  export let message
-  onMount(async () => {
-    let data = await (await fetch('/api')).json()
-    message = data.msg
-    console.log('MESSAGE: ', message)
-  })
+  import 'bulma/css/bulma.css'
+  import { fade, fly } from 'svelte/transition';
+
+  let greet = false;
+  const greeting = () => {greet = true}
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <h2>{message}</h2>
-  <h3>Change me!</h3>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+{ #if greet}
+<main in:fade class="hero is-primary">
+  <div class="hero-body">
+    <h1 class="title">Happy Birthday!</h1>
+    <h2 class="subtitle">Dear 爱爱美美 </h2>
+  </div>
 </main>
+{ /if }
+
+<div class="foot">
+  <button class="button is-large is-rounded is-warning" on:click={greeting}>
+      <span class="icon is-large">
+        <i class="fa fa-forumbee"></i>
+      </span>
+  </button>
+
+</div>
 
 <style>
-  main {
+  .foot {
+    position: fixed;
+    bottom: 10px;
+    width: 100%;
     text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
   }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+  
 </style>
